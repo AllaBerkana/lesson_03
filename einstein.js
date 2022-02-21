68d1e4e209cb27c66787b34989de394d105f4b03
AllaBerkana <allaberkana@gmail.com>
2022-02-21 19:33:14 +0200

    E=mc2

---
+++ einstein.js
@@ -0,0 +1,15 @@
+"use strict"
+
+{
+const myWeight = 55;
+const speedLight = 3e8;
+
+const energy1 = myWeight * speedLight**2;
+
+const energy2 = myWeight * Math.pow(speedLight, 2);
+
+console.log(energy1);
+console.log(energy2);
+
+
+}
